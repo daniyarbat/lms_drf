6ce03d95e056cbd586b6342a2af0dc71cc1029dd
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2024-02-15 20:44:05 +0600

    24.1 additional users viewset

--- config/settings.py
+++ config/settings.py
@@ -10,17 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-u^j!j6-b!0@bv1&2_h9zo65c!&8xu+p$5qj*zu@@g7rbskxwuy'
+# SECRET_KEY = os.getenv('DRF_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,11 +86,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lms',  # Название БД
-        'USER': 'postgres',  # Пользователь для подключения
-        'PASSWORD': '11111',  # Пароль для этого пользователя
-        'HOST': '127.0.0.1',  # Адрес, на котором развернут сервер БД
-        'PORT': 5432,  # Порт, на котором работает сервер БД
+        'NAME': 'lms',
+        'USER': 'postgres',
+        'PASSWORD': '11111',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+        # 'NAME': os.getenv('DRF_DB_NAME'),
+        # 'USER': os.getenv('DRF_DB_USER'),
+        # 'PASSWORD': os.getenv('DRF_DB_PASSWORD'),
     }
 }
 
@@ -112,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
