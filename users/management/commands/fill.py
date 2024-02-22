c15ba1827c87c8ab64e89f1589cf438f08f737a6
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2024-02-22 21:26:51 +0600

    24.2 custom command fix

--- users/management/commands/fill.py
+++ users/management/commands/fill.py
@@ -17,13 +17,21 @@ class Command(BaseCommand):
         user1, created = User.objects.get_or_create(email='test1@sky.pro')
         user2, created = User.objects.get_or_create(email='test2@sky.pro')
 
-        # Создаём платежи
+        # Создаем курсы
+        course1, created = Course.objects.get_or_create(title='Название курса 1')
+        course2, created = Course.objects.get_or_create(title='Название курса 2')
+
+        # Создаем уроки и связываем их с курсами
+        lesson1, created = Lesson.objects.get_or_create(title='Название урока 1', course=course1)
+        lesson2, created = Lesson.objects.get_or_create(title='Название урока 2', course=course2)
+
+        # Создаем платежи
         payment1 = Payment.objects.create(
             payer=user1,
             date_of_payment=datetime.datetime.now().date(),
             amount=10000,
             payment_type='cash',
-            payed_course=Course.objects.get(pk=2),
+            payed_course=course1,
         )
 
         payment2 = Payment.objects.create(
@@ -31,14 +39,15 @@ class Command(BaseCommand):
             date_of_payment=datetime.datetime.now().date(),
             amount=100000,
             payment_type='bank',
-            payed_lesson=Lesson.objects.get(pk=1),
+            payed_lesson=lesson1,
         )
 
         payment3 = Payment.objects.create(
+            payer=user1,
             date_of_payment=datetime.datetime.now().date(),
             amount=50000,
             payment_type='bank',
-            payed_lesson=Lesson.objects.get(pk=2),
+            payed_lesson=lesson2,
         )
 
         self.stdout.write(self.style.SUCCESS('Объекты оплаты успешно загружены'))
